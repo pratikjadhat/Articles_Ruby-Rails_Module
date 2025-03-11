ef2d4f9e141ef5b5ca112d21074c6de4b7a8b964
Pratik Jadhar <pratikjadhar2326@gmail.com>
2025-03-12 02:40:17 +0530

    Create validation of regex for email and validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
- 
-end
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 100 }
+  VALID_EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105},
+  format: {with: VALID_EMAIL_REGEX}
+end 
